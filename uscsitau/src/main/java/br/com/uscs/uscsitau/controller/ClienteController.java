8532e8e4fb2f72035557308b0f65505786766fa9
gabriel fonte <fontegabriel@gmail.com>
2020-05-24 15:55:49 -0300

    Update controller mapping

--- uscsitau/src/main/java/br/com/uscs/uscsitau/controller/ClienteController.java
+++ uscsitau/src/main/java/br/com/uscs/uscsitau/controller/ClienteController.java
@@ -1,6 +1,7 @@
 package br.com.uscs.uscsitau.controller;
 
 import java.util.List;
+import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -16,15 +17,15 @@ import br.com.uscs.uscsitau.repository.ClienteRepository;
 
 
 @RestController
-@RequestMapping(value="/cadastrados")
+@RequestMapping(value="/clientes")
 
 public class ClienteController {
 
     @Autowired
     ClienteRepository clienteRepository;
 
-    @GetMapping("/clientes")
-    public List<Cliente> listacCiesntes(){
+    @GetMapping("/listaClientes")
+    public List<Cliente> listaClientes(){
         return (List<Cliente>) clienteRepository.findAll();
     }
     
@@ -32,25 +33,32 @@ public class ClienteController {
     public clientes listaclienteunico(@PathVariable(value="cpf_cnpj") String cpf_cnpj)  {
     	return (clientes) clienteRepository.findById(cpf_cnpj);
     }*/ //Pesquisar sobre pesquisa de apenas um cliente
-    
-    @PostMapping("/clientes")
-    public Cliente salvaclientes(@RequestBody Cliente cadastro) {
-    	System.out.println("Cliente cadastrado com sucesso"); //Apenas parece no terminal
-		return clienteRepository.save(cadastro);
+
+
+    @PostMapping("/salvaClientes")
+    public Cliente salvaClientes(@RequestBody Cliente cadastro) {
+
+        List<Cliente> clientes = (List<Cliente>) clienteRepository.findAll();
+
+        clientes.forEach(item -> {
+            if(item.getCpf_cnpj().equals(cadastro.getCpf_cnpj())) {
+                System.out.println("Cliente com cpf/cnpj " + cadastro.getCpf_cnpj() + " já cadastrado");
+            }
+        });
+
+
+    	System.out.println("Cliente "+ cadastro.getNome() +" cadastrado com sucesso"); ////// Apenas parece no terminal
+        return clienteRepository.save(cadastro);
     }
     
-    @DeleteMapping("/clientes") //Se colocado apenas o CPF o clinete é deletado! Forma de se colocar no Postman  {"cpf_cnpj": "445.000.000-15"}
-    public void deletaclientes(@RequestBody Cliente cadastro) {
+    @DeleteMapping("/deletaClientes") //Se colocado apenas o CPF o clinete é deletado! Forma de se colocar no Postman  {"cpf_cnpj": "445.000.000-15"}
+    public void deletaClientes(@RequestBody Cliente cadastro) {
     	clienteRepository.delete(cadastro);
     }
     
-    @PutMapping("/clientes") //Utilizado para atualização de cadastros
-    public Cliente atualizaclientes(@RequestBody Cliente cadastro) {
+    @PutMapping("/atualizaClientes") //Utilizado para atualização de cadastros
+    public Cliente atualizaClientes(@RequestBody Cliente cadastro) {
     	return clienteRepository.save(cadastro);
     }
-		
-  
-    
-    
 
 }
--- uscsitau/src/main/java/br/com/uscs/uscsitau/model/Cliente.java
+++ uscsitau/src/main/java/br/com/uscs/uscsitau/model/Cliente.java
@@ -39,4 +39,68 @@ public class Cliente implements Serializable {
     String incr_estadual;
     @Column(value = "num_conta")
     String num_conta;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf_cnpj() {
+        return cpf_cnpj;
+    }
+
+    public void setCpf_cnpj(String cpf_cnpj) {
+        this.cpf_cnpj = cpf_cnpj;
+    }
+
+    public String getTipo_de_cliente() {
+        return tipo_de_cliente;
+    }
+
+    public void setTipo_de_cliente(String tipo_de_cliente) {
+        this.tipo_de_cliente = tipo_de_cliente;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public double getRenda() {
+        return renda;
+    }
+
+    public void setRenda(double renda) {
+        this.renda = renda;
+    }
+
+    public String getRazao_social() {
+        return razao_social;
+    }
+
+    public void setRazao_social(String razao_social) {
+        this.razao_social = razao_social;
+    }
+
+    public String getIncr_estadual() {
+        return incr_estadual;
+    }
+
+    public void setIncr_estadual(String incr_estadual) {
+        this.incr_estadual = incr_estadual;
+    }
+
+    public String getNum_conta() {
+        return num_conta;
+    }
+
+    public void setNum_conta(String num_conta) {
+        this.num_conta = num_conta;
+    }
 }
