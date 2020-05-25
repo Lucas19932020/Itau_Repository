ac51f003fa1207ade634e6e96151d771c3040057
gabriel fonte <fontegabriel@gmail.com>
2020-05-24 22:06:04 -0300

    Adding credit and debit

--- uscsitau/src/main/java/br/com/uscs/uscsitau/controller/ContaController.java
+++ uscsitau/src/main/java/br/com/uscs/uscsitau/controller/ContaController.java
@@ -1,17 +1,15 @@
 package br.com.uscs.uscsitau.controller;
 
-import java.util.List;
-import java.util.Random;
-
 import br.com.uscs.uscsitau.controller.dto.ContaDTO;
 import br.com.uscs.uscsitau.errorhandling.AppException;
 import br.com.uscs.uscsitau.errorhandling.ErrorCode;
+import br.com.uscs.uscsitau.model.ContaVO;
+import br.com.uscs.uscsitau.repository.ContaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import br.com.uscs.uscsitau.model.ContaVO;
-import br.com.uscs.uscsitau.repository.ContaRepository;
+import java.util.List;
 
 @RestController
 @RequestMapping(value="/conta")
@@ -23,6 +21,66 @@ public class ContaController {
 	@GetMapping("/lista")
 	public List<ContaVO> listaContas(){
 		return (List<ContaVO>) contaRepository.findAll();
+	}
+
+	@PostMapping("/credito")
+	public ResponseEntity creditarConta(@RequestBody ContaDTO contaDTO){
+		try {
+
+			String num_conta;
+
+			num_conta = contaDTO.getNum_conta().replaceAll("/\\D/g", "");
+
+			num_conta = contaDTO.getNum_conta().replaceAll("([0-9]{8})([0-9]{1})", "$1-$2");
+
+			if (contaRepository.getContaByNumConta(num_conta).isEmpty()) {
+				return ResponseEntity.status(404).body(new AppException(ErrorCode.ACCOUNT_NOT_FOUND));
+			}
+
+			ContaVO contaVO = contaRepository.getContaByNumConta(num_conta).get(0);
+
+			contaVO.setSaldo(contaVO.getSaldo() + contaDTO.getCredito());
+
+			contaRepository.save(contaVO);
+
+			return ResponseEntity.ok().body(contaVO);
+
+		} catch (Exception ex) {
+			return ResponseEntity.status(500).body(new AppException(ErrorCode.BAD_REQUEST));
+		}
+
+	}
+
+	@PostMapping("/debito")
+	public ResponseEntity debitarConta(@RequestBody ContaDTO contaDTO){
+		try {
+
+			String num_conta;
+
+			num_conta = contaDTO.getNum_conta().replaceAll("/\\D/g", "");
+
+			num_conta = contaDTO.getNum_conta().replaceAll("([0-9]{8})([0-9]{1})", "$1-$2");
+
+			if (contaRepository.getContaByNumConta(num_conta).isEmpty()) {
+				return ResponseEntity.status(404).body(new AppException(ErrorCode.ACCOUNT_NOT_FOUND));
+			}
+
+			ContaVO contaVO = contaRepository.getContaByNumConta(num_conta).get(0);
+
+			if (contaVO.getSaldo() - contaDTO.getDebito() < 0) {
+				return ResponseEntity.status(404).body(new AppException(ErrorCode.INSUFFICIENT_FUNDS));
+			} else {
+				contaVO.setSaldo(contaVO.getSaldo() - contaDTO.getDebito());
+			}
+
+			contaRepository.save(contaVO);
+
+			return ResponseEntity.ok().body(contaVO);
+
+		} catch (Exception ex) {
+			return ResponseEntity.status(500).body(new AppException(ErrorCode.BAD_REQUEST));
+		}
+
 	}
 
 	@GetMapping("/{num_conta}")
@@ -43,7 +101,6 @@ public class ContaController {
 			return ResponseEntity.ok().body(contaCorrenteVO);
 
 		} catch (Exception ex) {
-
 			return ResponseEntity.status(500).body(new AppException(ErrorCode.BAD_REQUEST));
 		}
 	}
--- uscsitau/src/main/java/br/com/uscs/uscsitau/controller/dto/ContaDTO.java
+++ uscsitau/src/main/java/br/com/uscs/uscsitau/controller/dto/ContaDTO.java
@@ -6,6 +6,8 @@ public class ContaDTO {
     private String agencia;
     private Integer dac;
     private double saldo;
+    private double credito;
+    private double debito;
 
     public String getNum_conta() {
         return num_conta;
@@ -37,5 +39,21 @@ public class ContaDTO {
 
     public void setSaldo(double saldo) {
         this.saldo = saldo;
+    }
+
+    public double getCredito() {
+        return credito;
+    }
+
+    public void setCredito(double credito) {
+        this.credito = credito;
+    }
+
+    public double getDebito() {
+        return debito;
+    }
+
+    public void setDebito(double debito) {
+        this.debito = debito;
     }
 }
